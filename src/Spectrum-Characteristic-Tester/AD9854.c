c6c5241264dbc7d85f839c2d4c025fdcd3ddd93f
ctlvie <ctlvie@gmail.com>
2018-11-22 12:43:24 +0800

    回退至并行模式

--- src/Spectrum-Characteristic-Tester/AD9854.c
+++ src/Spectrum-Characteristic-Tester/AD9854.c
@@ -1,223 +1,184 @@
 #include<msp430f5529.h>
 #include"AD9854.h"
 
-//******************************************************************************
-
-unsigned char AD9854_Reg_PHA1[2]	={0x00,0x00};
-unsigned char AD9854_Reg_PHA2[2]	={0x00,0x00};
-unsigned char AD9854_Reg_FRE1[6]	={0x00,0xF0,0x00,0x00,0x00,0x00};
-unsigned char AD9854_Reg_FRE2[6]	={0x80,0x00,0x00,0x00,0x00,0x00};
-unsigned char AD9854_Reg_DELTA[6]	={0x00,0x00,0x00,0x10,0x00,0x00};
-unsigned char AD9854_Reg_UPDATA[4]	={0x00,0x00,0x00,0x00};
-unsigned char AD9854_Reg_RAMP_CLK[3]={0x00,0x00,0x01};
-unsigned char AD9854_Reg_CTR_REG[4]	={	Bin(00010000),       //comp pd dowm  ctr[28]=0 //qac pd down ctr[26]=0
-                                        Bin(01000110),       //no pll pass ctr[21]=0  //multi times 6 ctr[19-16]=00110  
-                                        Bin(00100100),       //ext update ctr[8]=0    //Ramped FSK 
-                                        Bin(01000000) };     //pass inv CR[6]=1//osk EN ctr[5]=0 //msb first ctr[1]=0 //no sdo active ctr[0]=0
-unsigned char AD9854_Reg_I_MUL[2]	={0x00,0x00};
-unsigned char AD9854_Reg_Q_MUL[2]	={0x00,0x00};
-unsigned char AD9854_Reg_SHAPED[1]	={0x00};
-unsigned char AD9854_Reg_Q_DAC[2]	={0x00,0x00};
-
-////////////////////////////////////////////////////////////////////
-//                                                                //
-//                             BASIC FUNCTION                     //
-//                                                                //
-////////////////////////////////////////////////////////////////////
-
-//MCLK的频率为8000KHz的条件下延时ms个毫秒
-void DELAY_AD9854_MS (unsigned int ms)
+#include<msp430f5529.h>
+#include"AD9854.h"
+ulong  Freq_mult_ulong  = 1005268;
+double Freq_mult_doulle = 1005267.773966629;
+uchar FreqWord[6];               //6个字节频率控制字
+//=================================================================
+// 函数名称 ：void delay (uint us)
+// 函数功能 ：us级延时,大概2~4 us
+// 入口参数 ：us  延时时间的长短
+// 出口参数 ：无
+//=================================================================
+//====================================================================================
+//函数名称:void AD9854_Init(void)
+//函数功能:AD9854初始化
+//入口参数:无
+//出口参数:无
+//====================================================================================
+void AD9854_Init(void)
 {
-	unsigned int iq0,iq1 ;
-	for ( iq0 = ms ; iq0 > 0 ;iq0 --)
-	for ( iq1 = LOOPCNT  ; iq1 > 0 ;iq1 --) ;
+    DIROUT_AD9854_DataBus;
+    DIROUT_AD9854_AdrBus;
+    DIROUT_RD;
+    DIROUT_WR;
+    DIROUT_RST;
+    DIROUT_UDCLK;
+    SET_9854WR;//将读、写控制端口设为无效
+    SET_9854RD;
+    CLR_9854UDCLK;
+    SET_9854RST;                 //复位AD9854
+    CLR_9854RST;
+
+    AD9854_WR_Byte(0x1d,0x10);      //关闭比较器
+    AD9854_WR_Byte(0x1e,CLK_Set);      //设置系统时钟倍频
+    AD9854_WR_Byte(0x1f,0x00);     //设置系统为模式0，由外部更新
+    AD9854_WR_Byte(0x20,0x60);     //设置为可调节幅度，取消插值补偿
+
+    SET_9854UDCLK;                     //更新AD9854输出
+    CLR_9854UDCLK;
 }
-
-//******************************************************************************
-//函数名：SpisendByte(Uchar dat)
-//输入：发送的数据
-//输出：无
-//功能描述：SPI发送一个字节
-//******************************************************************************
-Uchar sendByte_AD9854(Uchar dat)
+//====================================================================================
+//函数名称:void Freq_convert(long Freq)
+//函数功能:正弦信号频率数据转换
+//入口参数:Freq   需要转换的频率，取值从0~SYSCLK/2
+//出口参数:无   但是影响全局变量FreqWord[6]的值
+//说明：   该算法位多字节相乘算法，有公式FTW = (Desired Output Frequency × 2N)/SYSCLK
+//         得到该算法，其中N=48，Desired Output Frequency 为所需要的频率，即Freq，SYSCLK
+//         为可编程的系统时钟，FTW为48Bit的频率控制字，即FreqWord[6]
+//====================================================================================
+void Freq_convert(long Freq)
 {
-//----------------------以下是模拟SPI时序方式-----------------------------------
-	Uchar i,temp;
-	temp = 0;	
-	AD9854_SCLK_DOWN;
-	for(i=0; i<8; i++)
-	{
-		if(dat & 0x80)
-		{
-	     AD9854_SDIO_UP;
-		}
-		else 
-		{
-		 AD9854_SDIO_DOWN;
-		}
-		dat <<= 1;
-	        AD9854_SCLK_UP; 
-		temp <<= 1;
-	if(AD9854_SDO_IN)temp++;  //读取MISO状态
-		AD9854_SCLK_DOWN;
-	}
-	return temp;
+    ulong FreqBuf;
+    ulong Temp=Freq_mult_ulong;
+    uchar Array_Freq[4];                 //将输入频率因子分为四个字节
+    Array_Freq[0]=(uchar)Freq;
+    Array_Freq[1]=(uchar)(Freq>>8);
+    Array_Freq[2]=(uchar)(Freq>>16);
+    Array_Freq[3]=(uchar)(Freq>>24);
+
+    FreqBuf=Temp*Array_Freq[0];
+    FreqWord[0]=FreqBuf;
+    FreqBuf>>=8;
+
+    FreqBuf+=(Temp*Array_Freq[1]);
+    FreqWord[1]=FreqBuf;
+    FreqBuf>>=8;
+
+    FreqBuf+=(Temp*Array_Freq[2]);
+    FreqWord[2]=FreqBuf;
+    FreqBuf>>=8;
+
+    FreqBuf+=(Temp*Array_Freq[3]);
+    FreqWord[3]=FreqBuf;
+    FreqBuf>>=8;
+
+    FreqWord[4]=FreqBuf;
+    FreqWord[5]=FreqBuf>>8;
 }
-
-
-//******************************************************************************
-//函数名：writeSPIBurstReg_AD9854(Uchar addr, Uchar *buffer, Uchar count)
-//输入：地址，写入缓冲区，写入个数
-//输出：无
-//功能描述：SPI连续写配置寄存器
-//******************************************************************************
-void writeSPIBurstReg_AD9854(Uchar addr, Uchar *buffer, Uchar count)
+//====================================================================================
+//函数名称:void AD9854_SetSine(ulong Freq,uint Shape)
+//函数功能:AD9854正弦波产生程序
+//入口参数:Freq   频率设置，取值范围为0~(1/2)*SYSCLK
+//         Shape  幅度设置. 为12 Bit,取值范围为(0~4095) ,取值越大,幅度越大
+//出口参数:无
+//====================================================================================
+void AD9854_SetSine(ulong Freq,uint Shape)
 {
-    Uchar i,temp;
-    temp = (addr & 0x0F); 				// Send Write address
-	AD9854_CS_DOWN;
-	while (AD9854_SDO_IN);
-	sendByte_AD9854(temp);
-    for (i = 0; i < count; i++)
+    uchar count;
+    uchar Adress;
+
+    Adress = 0x04;                        //选择频率控制字地址的初值
+
+    Freq_convert(Freq);                //频率转换
+
+    for(count=6;count>0;)             //写入6字节的频率控制字
     {
-        sendByte_AD9854(buffer[i]);
+        AD9854_WR_Byte(Adress++,FreqWord[--count]);
     }
-    AD9854_CS_UP;
-}
 
-//******************************************************************************
-//函数名：void readSPIBurstReg_AD9854(Uchar addr, Uchar *buffer, Uchar count)
-//输入：地址，读出数据后暂存的缓冲区，读出配置个数
-//输出：无
-//功能描述：SPI连续读配置寄存器
-//******************************************************************************
-void readSPIBurstReg_AD9854(Uchar addr, Uchar *buffer, Uchar count)
+    AD9854_WR_Byte(0x21,Shape>>8);    //设置I通道幅度
+    AD9854_WR_Byte(0x22,(uchar)(Shape&0xff));
+
+    AD9854_WR_Byte(0x23,Shape>>8);    //设置Q通道幅度
+    AD9854_WR_Byte(0x24,(uchar)(Shape&0xff));
+
+    SET_9854UDCLK;                    //更新AD9854输出
+        CLR_9854UDCLK;
+}
+//====================================================================================
+//函数名称:void Freq_doublt_convert(double Freq)
+//函数功能:正弦信号频率数据转换
+//入口参数:Freq   需要转换的频率，取值从0~SYSCLK/2
+//出口参数:无   但是影响全局变量FreqWord[6]的值
+//说明：   有公式FTW = (Desired Output Frequency × 2N)/SYSCLK得到该函数，
+//         其中N=48，Desired Output Frequency 为所需要的频率，即Freq，SYSCLK
+//         为可编程的系统时钟，FTW为48Bit的频率控制字，即FreqWord[6]
+//注意：   该函数与上面函数的区别为该函数的入口参数为double，可使信号的频率更精确
+//         谷雨建议在100HZ以下用本函数，在高于100HZ的情况下用函数void Freq_convert(long Freq)
+//====================================================================================
+void Freq_double_convert(double Freq)
 {
-  unsigned int i,temp;
-  temp = ((addr & 0x0F) | 0x80);  		// Send Read address
-  AD9854_CS_DOWN;
-  while (AD9854_SDO_IN);
-  sendByte_AD9854(temp); 
-  for (i = 0; i < count; i++) 
-	{
-      buffer[i] = sendByte_AD9854(0);
+   ulong Low32;
+   uint  High16;
+    double Temp=Freq_mult_doulle;                   //23ca99为2的48次方除以120M
+   Freq*=(double)(Temp);
+//  1 0000 0000 0000 0000 0000 0000 0000 0000 = 4294967295
+   High16 = (int)(Freq/4294967295);                  //2^32 = 4294967295
+   Freq -= (double)High16*4294967295;
+   Low32 = (ulong)Freq;
+
+    FreqWord[0]=Low32;
+    FreqWord[1]=Low32>>8;
+    FreqWord[2]=Low32>>16;
+    FreqWord[3]=Low32>>24;
+    FreqWord[4]=High16;
+    FreqWord[5]=High16>>8;
+}
+//====================================================================================
+//函数名称:void AD9854_SetSine_double(double Freq,uint Shape)
+//函数功能:AD9854正弦波产生程序
+//入口参数:Freq   频率设置，取值范围为0~1/2*SYSCLK
+//         Shape  幅度设置. 为12 Bit,取值范围为(0~4095)
+//出口参数:无
+//====================================================================================
+void AD9854_SetSine_double(double Freq,uint Shape)
+{
+    uchar count=0;
+    uchar Adress;
+
+    Adress=0x04;                             //选择频率控制字1地址的初值
+
+    Freq_double_convert(Freq);                 //频率转换
+
+    for(count=6;count>0;)                       //写入6字节的频率控制字
+    {
+        AD9854_WR_Byte(Adress++,FreqWord[--count]);
     }
-  AD9854_CS_UP;
+
+    AD9854_WR_Byte(0x21,Shape>>8);    //设置I通道幅度
+    AD9854_WR_Byte(0x22,(uchar)(Shape&0xff));
+
+    AD9854_WR_Byte(0x23,Shape>>8);    //设置Q通道幅度
+    AD9854_WR_Byte(0x24,(uchar)(Shape&0xff));
+
+    SET_9854UDCLK;                    //更新AD9854输出
+        CLR_9854UDCLK;
 }
 
-//函数功能:更新设置
-//输入参数:无
-//******************************************************************
-void configAD9854(void)
+//====================================================================================
+//函数名称:void AD9854_WR_Byte(uchar addr,uchar dat)
+//函数功能:AD9854并行口写入数据
+//入口参数:addr     6位地址
+//         dat      写入的数据
+//出口参数:无
+//====================================================================================
+void AD9854_WR_Byte(uchar addr,uchar dat)
 {
-  	unsigned i=100 ;        //
-	AD9854_UPDATE_OUT;
-	AD9854_UPDATE_UP ;      //
-	while(i--) ;
-	AD9854_UPDATE_DOWN ;
+    AD9854_AdrBus  = addr;
+    AD9854_DataBus = dat;
+    CLR_9854WR;
+    SET_9854WR;
 }
-//******************************************************************
-
-//函数功能:复位
-//输入参数:无
-//******************************************************************
-void resetAD9854(void)
-{
-	unsigned i=100 ;        //
-	AD9854_IO_RESET_UP ;
-	while(i--) ;
-	AD9854_IO_RESET_DOWN ;
-}
-//******************************************************************
-
-//函数功能:AD9854初始化 写控制寄存器
-//输入参数:无
-//硬件说明：
-//******************************************************************
-void initAD9854(void)
-{
-	HARDWARE_AD9854 ;       //通过宏定义设置
-	AD9854_IO_serial;		//串行模式
-	AD9854_OSC_ON;			//打开时钟
-	AD9854_CS_DOWN ;        //片选	
-	AD9854_RESET_UP;	
-	DELAY_AD9854_MS(10);
-	AD9854_RESET_DOWN;		//主复位	
-	resetAD9854();
-	AD9854_OSC_OFF;			//关闭时钟，更新配置
-	writeSPIBurstReg_AD9854(AD9854_Addr_CTR_REG,		//写控制寄存器
-						AD9854_Reg_CTR_REG, AD9854_Length_CTR_REG);			
-	AD9854_OSC_ON;			//打开时钟，更新配置
-	DELAY_AD9854_MS(10);
-	AD9854_UPDATE_OUT;
-	AD9854_UPDATE_DOWN;		//配置更新时钟为输出
-}
-
-//******************************************************************
-//函数功能：写 Frequency Tuning Word #1
-//输入参数: 
-//写入地址字节数:1 Byte
-//写入数据字节数:6 Bytes 
-//******************************************************************
-void writeAD9854_Freq1(void)
-{ 	
-	writeSPIBurstReg_AD9854(AD9854_Addr_FRE1,		//写频率控制寄存器1
-						AD9854_Reg_FRE2, AD9854_Length_FRE1);
-	 configAD9854();
-
-}
-
-//******************************************************************
-//函数功能：设置三角扫频
-//输入参数:
-//Serial Register Address :2，3，4，6
-//写入地址字节数:1 Byte
-//写入数据字节数:6 Bytes 
-//******************************************************************
-void writeAD9854_FreqSW(void)
-{ 	
-	writeSPIBurstReg_AD9854(AD9854_Addr_FRE1,		//写频率控制寄存器1
-						AD9854_Reg_FRE1, AD9854_Length_FRE1);
-		  resetAD9854();
-	writeSPIBurstReg_AD9854(AD9854_Addr_FRE2,		//写频率控制寄存器2
-						AD9854_Reg_FRE2, AD9854_Length_FRE2);
-		  resetAD9854();
-	writeSPIBurstReg_AD9854(AD9854_Addr_DELTA,		//写频率增量寄存器
-						AD9854_Reg_DELTA, AD9854_Length_DELTA);
-		  resetAD9854();
-	writeSPIBurstReg_AD9854(AD9854_Addr_RAMP_CLK,	//写扫频时钟寄存器
-						AD9854_Reg_RAMP_CLK, AD9854_Length_RAMP_CLK);
-	  	configAD9854();
-}
-
-
-//******************************************************************************
-//系统初始化
-//******************************************************************************
-/*
-void Initsys()
-{
-   unsigned int iq0;
-   _DINT();
-   BCSCTL1 &=~XT2OFF;
-   do
-   {
-      IFG1 &= ~OFIFG;		    	// 清除振荡器失效标志
-  for (iq0 = 0xFF; iq0 > 0; iq0--);	// 延时，等待XT2起振
-   }
-   while ((IFG1 & OFIFG) != 0);		// 判断XT2是否起振		
-   BCSCTL2 |=SELM1+SELS;			//MCLK为8MHz，SMCLK为8MHz ;                
-}
-
-void main( void )
-{
-     WDTCTL = WDTPW + WDTHOLD ; 
-     Initsys();   
-     initAD9854();
- 	writeAD9854_FreqSW();
-	//writeAD9854_Freq1();
-     while(1) ;
-
-}
-
-*/
--- src/Spectrum-Characteristic-Tester/AD9854.h
+++ src/Spectrum-Characteristic-Tester/AD9854.h
@@ -1,123 +1,129 @@
+/*
+ * AD9854.h
+ *
+ *  Created on: 2018年11月15日
+ *      Author: Admin
+ */
 
 #ifndef AD9854_H_
 #define AD9854_H_
-#define  uclong   unsigned long
-#define  Uchar    unsigned char
-#define LongToBin(n) (((n>>21)&0x80)|((n>>18)&0x40)|((n>>15)&0x20)|((n>>12)&0x10)|((n>>9)&0x08)|((n>>6)&0x04)|((n>>3)&0x02)|((n)&0x01)) 
-#define Bin(n) LongToBin(0x##n##l)
+#ifndef uchar
+#define uchar unsigned char
+#endif
+#ifndef ulong
+#define ulong unsigned long
+#endif
+#ifndef uint
+#define uint unsigned int
+#endif
+//extern uchar FreqWord[6];              //6个字节频率控制字
+//**********************以下为系统时钟以及其相关变量设置**************************
 
-/*=======================================================
-    接线方式:
-    1. CS   -> 
-    2. SCLK    -> 
-    3. UPDATE  -> 
-    4. SDIO  -> 
-    5. SDO -> 
-    6. RESET -> 
-    7. Serial ->
-    8. OSC
-    8. IORESET -> 
-========================================================*/
+/*
+      此处根据自己的需要设置系统时钟以及与其相关的因子，一次需且只需开启一个
+      CLK_Set为时钟倍频设置，可设置4~20倍倍频，但最大不能超过300MHZ
+      Freq_mult_ulong和Freq_mult_doulle均为2的48次方除以系统时钟，一个为长整形，一个为双精度型
+*/
+
+/*
+#define      CLK_Set            4
+const ulong  Freq_mult_ulong  = 3518437;
+const double Freq_mult_doulle = 3518437.2088832;
+*/
+
+/*
+#define      CLK_Set            5
+const ulong  Freq_mult_ulong  = 2814750;
+const double Freq_mult_doulle = 2814749.76710656;
+*/
+
+/*
+#define      CLK_Set            6
+const ulong  Freq_mult_ulong  = 2345625;
+const double Freq_mult_doulle = 2345624.80592213;
+*/
+
+/*
+#define      CLK_Set            7
+const ulong  Freq_mult_ulong  = 2010536;
+const double Freq_mult_doulle = 2010535.54793326;
+*/
+
+/*
+#define      CLK_Set            8
+const ulong  Freq_mult_ulong  = 1759219;
+const double Freq_mult_doulle = 1759218.6044416;
+*/
+
+/*
+#define      CLK_Set            9
+const ulong  Freq_mult_ulong  = 1563750;
+const double Freq_mult_doulle = 1563749.87061476;
+*/
+
+/*
+#define      CLK_Set            10
+const ulong  Freq_mult_ulong  = 1407375;
+const double Freq_mult_doulle = 1407374.88355328;
+*/
+
+/*
+#define      CLK_Set            11
+const ulong  Freq_mult_ulong  = 1279432;
+const double Freq_mult_doulle = 1279431.712321164;
+*/
+
+/*
+#define      CLK_Set            12
+const ulong  Freq_mult_ulong  = 1172812;
+const double Freq_mult_doulle = 1172812.402961067;
+*/
+
+/*
+#define      CLK_Set            13
+const ulong  Freq_mult_ulong  = 1082596;
+const double Freq_mult_doulle = 1082596.064271754;
+*/
 
 
-//******************************************************************
-//端口操作
-#define IO_AD9854_1_OUT     P8OUT
-#define IO_AD9854_1_DIR     P8DIR
-#define IO_AD9854_2_OUT     P4OUT
-#define IO_AD9854_2_DIR     P4DIR
-#define IO_AD9854_2_IN      P4IN
-#define IO_AD9854_3_OUT     P7OUT
-#define IO_AD9854_3_DIR     P7DIR
-
-#define IO_AD9854_1_BIT_CS      BIT6
-#define IO_AD9854_1_BIT_RESET   BIT7
-#define IO_AD9854_2_BIT_SCLK    BIT6
-#define IO_AD9854_2_BIT_UPDATE  BIT7
-#define IO_AD9854_2_BIT_SDIO    BIT0
-#define IO_AD9854_2_BIT_SDO    BIT1
-#define IO_AD9854_2_BIT_IORESET BIT2
-#define IO_AD9854_3_BIT_OSC     BIT6
+#define      CLK_Set            14
+//extern ulong  Freq_mult_ulong  = 1005268;
+//extern double Freq_mult_doulle = 1005267.773966629;
 
 
-//第二版硬件
-#define AD9854_CS_UP       IO_AD9854_1_OUT |= IO_AD9854_1_BIT_CS
-#define AD9854_CS_DOWN     IO_AD9854_1_OUT &=~IO_AD9854_1_BIT_CS   	//片选
-#define AD9854_SCLK_UP     IO_AD9854_2_OUT |= IO_AD9854_2_BIT_SCLK
-#define AD9854_SCLK_DOWN   IO_AD9854_2_OUT &=~IO_AD9854_2_BIT_SCLK	//数据时钟
-#define AD9854_UPDATE_UP   IO_AD9854_2_OUT |= IO_AD9854_2_BIT_UPDATE
-#define AD9854_UPDATE_DOWN IO_AD9854_2_OUT &=~IO_AD9854_2_BIT_UPDATE	//更新时钟
-#define AD9854_UPDATE_OUT   IO_AD9854_2_DIR |= IO_AD9854_2_BIT_UPDATE
-#define AD9854_UPDATE_IN 	IO_AD9854_2_DIR &=~IO_AD9854_2_BIT_UPDATE	//更新时钟方向
-#define AD9854_SDIO_UP     IO_AD9854_2_OUT |= IO_AD9854_2_BIT_SDIO
-#define AD9854_SDIO_DOWN   IO_AD9854_2_OUT &=~IO_AD9854_2_BIT_SDIO	//数据输出
-#define AD9854_SDO_IN      (IO_AD9854_2_IN & IO_AD9854_2_BIT_SDO) 	//数据输入
-#define AD9854_IO_RESET_UP    IO_AD9854_2_OUT |= IO_AD9854_2_BIT_IORESET
-#define AD9854_IO_RESET_DOWN  IO_AD9854_2_OUT &=~IO_AD9854_2_BIT_IORESET	//SPI总线复位
-#define AD9854_RESET_UP    IO_AD9854_1_OUT |= IO_AD9854_1_BIT_RESET
-#define AD9854_RESET_DOWN  IO_AD9854_1_OUT &=~IO_AD9854_1_BIT_RESET   	//主复位
-#define AD9854_IO_parallel //P6OUT |= BIT7
-#define AD9854_IO_serial   //P6OUT &=~BIT7   	//串并选择
-#define AD9854_OSC_ON    	IO_AD9854_3_OUT |= IO_AD9854_3_BIT_OSC	
-#define AD9854_OSC_OFF  	IO_AD9854_3_OUT &=~IO_AD9854_3_BIT_OSC	//晶振控制
-#define  HARDWARE_AD9854   IO_AD9854_1_DIR |= IO_AD9854_1_BIT_CS+IO_AD9854_1_BIT_RESET; IO_AD9854_2_DIR |= IO_AD9854_2_BIT_SDIO+IO_AD9854_2_BIT_IORESET+IO_AD9854_2_BIT_SCLK; IO_AD9854_3_DIR |= IO_AD9854_3_BIT_OSC;IO_AD9854_3_OUT &=~IO_AD9854_3_BIT_OSC //AD9854_OSC_OFF
+/*
+#define      CLK_Set            15
+const ulong  Freq_mult_ulong  = 938250;
+const double Freq_mult_doulle = 938249.9223688533;
+*/
+//**************************修改硬件时要修改的部分********************************
+
+#define AD9854_DataBus              P3OUT             //P3BIT0 ~ BIT7 为数据总线, 对应 D0 ~ D7
+#define AD9854_AdrBus               P2OUT             //P2BIT0 ~ BIT7 为地址总线, 对应 A0 ~ A5
+#define DIROUT_AD9854_DataBus       P3DIR = 0xff      //数据线IO口设为输出
+#define DIROUT_AD9854_AdrBus        P2DIR = 0xff      //地址线IO口设为输出
+#define DIROUT_RD                   P4DIR |= BIT4;    //P4.4 -> RD
+#define DIROUT_WR                   P4DIR |= BIT5;    //P4.5 -> WR
+#define DIROUT_UDCLK                P4DIR |= BIT6;    //P4.6 -> UCLK
+#define DIROUT_RST                  P4DIR |= BIT7;    //P4.7 -> RST
+#define CLR_9854RD                  P4OUT &= ~BIT4;   //RD使能(低有效)
+#define SET_9854RD                  P4OUT |= BIT4;
+#define CLR_9854WR                  P4OUT &= ~BIT5;   //WR使能(低有效)
+#define SET_9854WR                  P4OUT |= BIT5;
+#define CLR_9854UDCLK               P4OUT &= ~BIT6;   //更新时钟
+#define SET_9854UDCLK               P4OUT |= BIT6;
+#define CLR_9854RST                 P4OUT &= ~BIT7;   //复位信号
+#define SET_9854RST                 P4OUT |= BIT7;
+//**************************以下部分为函数定义********************************
+
+void AD9854_WR_Byte(uchar addr,uchar dat);
+void AD9854_Init(void);
+void Freq_convert(long Freq);
+void AD9854_SetSine(ulong Freq,uint Shape);
+void Freq_double_convert(double Freq);
+void AD9854_SetSine_double(double Freq,uint Shape);
 
 
-////////////////////////////////////////////////////////////////////
-//                                                                //
-//                             INSTRUCTION BYTE                   //
-//                                                                //
-////////////////////////////////////////////////////////////////////
 
-//******************************************************************
-//寄存器地址 注意是串行地址
-#define	  AD9854_Addr_PHA1       0        	// 2 Bytes
-#define	  AD9854_Addr_PHA2       1        	// 2 Bytes 
-#define	  AD9854_Addr_FRE1       2        	// 6 Bytes	
-#define	  AD9854_Addr_FRE2       3        	// 6 Bytes
-#define	  AD9854_Addr_DELTA      4        	// 6 Bytes
-#define	  AD9854_Addr_UPDATA     5        	// 4 Bytes
-#define	  AD9854_Addr_RAMP_CLK   6        	// 3 Bytes
-#define	  AD9854_Addr_CTR_REG    7        	// 4 Bytes
-#define	  AD9854_Addr_I_MUL      8        	// 2 Bytes
-#define	  AD9854_Addr_Q_MUL      9        	// 2 Bytes
-#define	  AD9854_Addr_SHAPED     10       	// 1 Bytes
-#define	  AD9854_Addr_Q_DAC      11        	// 2 Bytes
-//******************************************************************
-
-//******************************************************************
-//寄存器长度
-#define	  AD9854_Length_PHA1       2        	// 2 Bytes
-#define	  AD9854_Length_PHA2       2        	// 2 Bytes 
-#define	  AD9854_Length_FRE1       6        	// 6 Bytes	
-#define	  AD9854_Length_FRE2       6        	// 6 Bytes
-#define	  AD9854_Length_DELTA      6        	// 6 Bytes
-#define	  AD9854_Length_UPDATA     4        	// 4 Bytes
-#define	  AD9854_Length_RAMP_CLK   3        	// 3 Bytes
-#define	  AD9854_Length_CTR_REG    4        	// 4 Bytes
-#define	  AD9854_Length_I_MUL      2        	// 2 Bytes
-#define	  AD9854_Length_Q_MUL      2        	// 2 Bytes
-#define	  AD9854_Length_SHAPED     1       		// 1 Bytes
-#define	  AD9854_Length_Q_DAC      2        	// 2 Bytes
-
-////////////////////////////////////////////////////////////////////
-//                                                                //
-//                          VARIABLES   DEFINITION                //
-//                                                                //
-////////////////////////////////////////////////////////////////////
-
-#define  FREQUENCY  8000     // MCLK的频率为8000KHz ,即1秒钟执行8M个时钟周期
-#define  LOOPBODY  8         //MSP430中一个for循环体耗费8个周期
-#define  LOOPCNT ( unsigned int )  (( FREQUENCY/LOOPBODY ))
-
-void DELAY_AD9854_MS (unsigned int ms);
-Uchar sendByte_AD9854(Uchar dat);
-void writeSPIBurstReg_AD9854(Uchar addr, Uchar *buffer, Uchar count);
-void readSPIBurstReg_AD9854(Uchar addr, Uchar *buffer, Uchar count);
-void configAD9854(void);
-void resetAD9854(void);
-void initAD9854(void);
-void writeAD9854_Freq1(void);
-void writeAD9854_FreqSW(void);
 
 #endif /* AD9854_H_ */
