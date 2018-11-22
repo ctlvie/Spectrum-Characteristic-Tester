7dc54977152dfac32a6e958cc5767e2bc0cd70ef
ctlvie <ctlvie@gmail.com>
2018-11-22 13:34:57 +0800

    更改IO分配

--- src/Spectrum-Characteristic-Tester/LCD12864.h
+++ src/Spectrum-Characteristic-Tester/LCD12864.h
@@ -14,30 +14,30 @@ Date		By			Version		Description
 
 /*=======================================================
     接线方式:
-    1. VSS -> GND
-    2. VDD -> +5V
-    3. VO  -> +5V ? 空脚
-    4. CS  -> CS
-    5. SID -> SID
-    6. SCLK -> SCLK
-    7~14.DB0~DB7 并行输入
-    15. PSB-> 串行接GND 并行接+5V
-    16. NC -> 空脚
-    17. RST -> +5V
-    18. VEE -> 空脚
-    19. BLA -> +5V
-    20. BLK -> GND
+    1. VSS ->       GND
+    2. VDD ->       +5V
+    3. VO  ->       NC
+    4. RS (CS)   -> CS      ->  P2.0
+    5. R/W (SID) -> SID     ->  P2.4
+    6. E (SCLK)  -> SCLK    ->  P2.5
+    7~14.DB0~DB7    并行输入
+    15. PSB->       串行接GND 并行接+5V
+    16. NC ->       NC
+    17. RST ->      +5V
+    18. VEE ->      NC
+    19. BLA ->      +5V
+    20. BLK ->      GND
 ========================================================*/
 
 #ifndef LCD12864_H_
 #define LCD12864_H_
 
-#define IO_LCD_DIR      P6DIR
-#define IO_LCD_OUT      P6OUT
-#define IO_LCD_IN       P6IN
+#define IO_LCD_DIR      P2DIR
+#define IO_LCD_OUT      P2OUT
+#define IO_LCD_IN       P2IN
 #define IO_LCD_BIT_CS   BIT0
-#define IO_LCD_BIT_SID  BIT1
-#define IO_LCD_BIT_SCLK BIT2
+#define IO_LCD_BIT_SID  BIT4
+#define IO_LCD_BIT_SCLK BIT5
 
 #define CS1     IO_LCD_OUT |=   IO_LCD_BIT_CS			
 #define CS0     IO_LCD_OUT &=   IO_LCD_BIT_CS
